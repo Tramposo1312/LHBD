863e84e6ff11feb27f65195664a46451bdd25525
Tramposo1312 <anasskharmoudi464@gmail.com>
2023-09-25 21:06:35 +0100

    hotfix

--- resources/roleplay/client.js
+++ resources/roleplay/client.js
@@ -33,11 +33,12 @@ addEventHandler("OnPedJackVehicle", function(event, ped, vehicle, seat) {
 
 // ===========================================================================
 addEventHandler("OnPedDeath", function(event, ped) {
-    ped.kill();
+	ped.health = 100;
+
 });
 
 addEventHandler("onPedSpawn", (event, gunDealerPed) => {
-    gunDealerPed.addAnimation(`${gunDealerAnim}.i3d`);
+    gunDealerPed.addAnimation(`breath01c.i3d`);
 });
 
 
@@ -52,42 +53,7 @@ addNetworkHandler("updateGoods", function(goodsData) {
 });
 
 addEventHandler("OnMapLoaded", function(event, client) {
-    db.query(`SELECT * FROM users WHERE username = '${client.name}'`);
-	let playerMoney = db.query(`SELECT money FROM users WHERE username = '${client.name}'`);
-    let checkUser = db.query(`SELECT * FROM users WHERE username = '${client.name}'`);
-	if (checkUser == ",,,") {
-		messageClient("Seems like you need to register, pal!", client, COLOUR_GREEN);
-		messageClient("To register, type /register <password>", client, COLOUR_GREEN);
-		console.log(checkUser);
-		return;
-	} else if (checkUser !== ",,,") {
-		messageClient("Look who's back...", client, COLOUR_AQUA);
-		messageClient('Use /login <password> before i touch you, stupid kid.', client, COLOUR_AQUA);
-		playerMoney = parseInt(playerMoney, 10) | 0;
-		hudPlayerMoney(playerMoney);
-		const RegisteredPlayer = true;
-
-		messageClient(`You will receive a paycheck of $${PaycheckMoney} every one hour ingame.`, client, COLOUR_GREEN);
-
-
-		setInterval(() => {
-			const theplayerMoney = db.query(`SELECT money FROM users WHERE username = '${client.name}'`);
-			hudPlayerMoney(theplayerMoney);
-		 }, 1000);
-
-
-		const paycheckInterval = 3600000;
-
-		setInterval(() => {
-
-            playerMoney += PaycheckMoney;
-            db.query(`UPDATE users SET money = ${playerMoney} WHERE username = '${client.name}'`);
-
-
-            messageClient(`You received a paycheck of $${PaycheckMoney}.`, client, COLOUR_GREEN);
-        }, paycheckInterval);
-
-    }
+  
   });
 
 
